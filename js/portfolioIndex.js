1e3bfa4d7e61b7193574af39885afd6466052f6d
drissmouchkila <mouchkiladriss2014@gmail.com>
2021-01-07 18:36:37 +0000

    eCommerce card styles

--- js/portfolioIndex.js
+++ js/portfolioIndex.js
@@ -36,3 +36,36 @@ $(document).ready(function (){
         $('.menu-btn i').toggleClass("active");
     });
 });
+// send email
+document.querySelector(".contactForm").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+    // get input values
+    let firstname = document.querySelector(".firstname").value;
+    let lastname = document.querySelector(".lastname").value;
+    let email = document.querySelector(".email").value;
+    let number = document.querySelector(".number").value;
+    let message = document.querySelector(".message").value;
+
+    sendEmail(firstname, lastname, email, number, message);
+
+    document.querySelector(".contactForm").reset();
+}
+
+function sendEmail(firstname, lastname, email, number, message) {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: 'mouchkiladriss2014@gmail.com',
+        Password: "ntvtwnztyiowwjdg",
+        To: 'mouchkiladriss2014@gmail.com',
+        From: 'mouchkiladriss2014@gmail.com',
+        Subject: firstname + '  ' +  lastname + '  ' +  'send you Message from portfolio',
+        Body: 'Bonjour: <br/>' +
+            ' <br/> Name: ' + firstname + ' ' + lastname +
+            ' <br/> Email: ' + email +
+            ' <br/> number: ' + number +
+            '<br/>  Message: <br/><br/> ' + message +
+            '<br/>',
+    }).then((message) => alert("mail sent successfully"));
+}
